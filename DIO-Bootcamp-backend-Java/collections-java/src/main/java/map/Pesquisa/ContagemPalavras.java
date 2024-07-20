2d4d0baf941df074f991dcced4b897919bbb6919
AndersonCSM <96142983+AndersonCSM@users.noreply.github.com>
2024-07-20 10:14:12 -0300

    +

--- DIO-Bootcamp-backend-Java/collections-java/src/main/java/map/Pesquisa/ContagemPalavras.java
+++ DIO-Bootcamp-backend-Java/collections-java/src/main/java/map/Pesquisa/ContagemPalavras.java
@@ -1,7 +1,36 @@
 package main.java.map.Pesquisa;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ContagemPalavras {
     private Map<String, Integer> palavras;
+
+    public ContagemPalavras() {
+        this.palavras = new HashMap<>();
+    }
+
+    public void addPalavra(String palavra, int contagem){
+        palavras.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra){
+        if (palavras.get(palavra) != null){
+            palavras.remove(palavra);
+        }
+    }
+
+    public void exibirPalavra(){
+        System.out.println(palavras);
+    }
+
+    public void maisFrequente(){
+        String frequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry: palavras.entrySet()){
+            if (entry.getValue() > maiorContagem){
+
+            }
+        }
+    }
 }
