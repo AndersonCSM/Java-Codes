1055315c7e96af7a451f335b311dec5397c1c979
AndersonCSM <96142983+AndersonCSM@users.noreply.github.com>
2024-07-20 11:52:18 -0300

    Exercicios finais collections

--- DIO-Bootcamp-backend-Java/collections-java/src/main/java/map/Pesquisa/ContagemPalavras.java
+++ DIO-Bootcamp-backend-Java/collections-java/src/main/java/map/Pesquisa/ContagemPalavras.java
@@ -20,17 +20,40 @@ public class ContagemPalavras {
         }
     }
 
-    public void exibirPalavra(){
-        System.out.println(palavras);
+    public int exibirContagem(){
+        int contagem = 0;
+        for (Integer c: palavras.values()){
+            contagem += c;
+        }
+        return contagem;
     }
 
-    public void maisFrequente(){
+    public String maisFrequente(){
         String frequente = null;
         int maiorContagem = 0;
         for (Map.Entry<String, Integer> entry: palavras.entrySet()){
             if (entry.getValue() > maiorContagem){
-
+                frequente = entry.getKey();
+                maiorContagem = entry.getValue();
             }
         }
+        return frequente;
     }
+
+    public static void main(String[] args) {
+    ContagemPalavras contagemLinguagens = new ContagemPalavras();
+
+    // Adiciona linguagens e suas contagens
+    contagemLinguagens.addPalavra("Java", 2);
+    contagemLinguagens.addPalavra("Python", 8);
+    contagemLinguagens.addPalavra("JavaScript", 1);
+    contagemLinguagens.addPalavra("C#", 6);
+
+    // Exibe a contagem total de linguagens
+    System.out.println("Existem " + contagemLinguagens.exibirContagem() + " palavras.");
+
+    // Encontra e exibe a linguagem mais frequente
+    String linguagemMaisFrequente = contagemLinguagens.maisFrequente();
+    System.out.println("A linguagem mais frequente é: " + linguagemMaisFrequente);
+  }
 }
